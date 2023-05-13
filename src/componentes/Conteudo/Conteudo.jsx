fa8eae2b323155f94b7a8ce290ee7c67d5a128c5
Nicolas <nickcintra@hotmail.com>
2023-05-12 21:41:19 -0300

    atualização dia 12 de maio

--- src/componentes/Conteudo/Conteudo.jsx
+++ src/componentes/Conteudo/Conteudo.jsx
@@ -3,13 +3,15 @@ import React from "react"
 import { Routes, Route } from 'react-router-dom'
 
 import Cliente from '../../views/Cliente'
-import Home from '../../views/Home'
+import Venda from '../../views/Venda'
+import Produto from '../../views/Produto'
 
 const Conteudo = props => (
     <main className="conteudo">
         <Routes>
             <Route path="/cliente" element={<Cliente />}/>
-            <Route path="/home" element={<Home />}/>
+            <Route path="/venda" element={<Venda />}/>
+            <Route path="/produto" element={<Produto />}/>  
         </Routes>
     </main>
 )
